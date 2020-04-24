3807c6445f549ea7819abceb2a9f748be9a9b3e2
kubab96 <61728764+kubab96@users.noreply.github.com>
2020-04-24 10:59:35 +0200

    Poprawiono koszyk oraz składanie zamówień

--- src/main/java/controler/DataLoader.java
+++ src/main/java/controler/DataLoader.java
@@ -36,19 +36,24 @@ public class DataLoader implements ApplicationRunner {
         adresRepository.save(new Adres("Krak�w", "Kr�lewska", "4", "10-111"));
         adresRepository.save(new Adres("Pozna�", "Lipowa", "17", "10-111"));
         
-      //  klientRepository.save(new Klient("Jan","Kowalski","jankowalski@gmail.com"));
+        var a1 = new Adres("Krosno", "Zielona", "1", "10-000");
+        var a2 = new Adres("Rzeszow", "D�uga", "19", "10-100");
+        
+      klientRepository.save(new Klient("Jan","Kowalski","jankowalski@gmail.com", a2));
        // klientRepository.save(new Klient("Adam","Nowak","adamnowak12@gmail.com"));             
       //  klientRepository.save(new Klient("Kinga","Sarna","ksarna23@gmail.com")); 
       //  klientRepository.save(new Klient("Piotr","�y�a","piotrus@gmail.com")); 
       //  klientRepository.save(new Klient("Mariusz","Wal�sa","walesa23@gmail.com")); 
-        
+
+      
         ksiazkaRepository.save(new Ksiazka("Teatr wskrzeszonych","kryminal, sensacja, thriller", "" , 21));
         ksiazkaRepository.save(new Ksiazka("Pan Tadeusz", "Adam Mickiewicz", "Poezja epicka" , 45));
         ksiazkaRepository.save(new Ksiazka("Przedwio�nie", "Stefan �eromski", "Powie��, Fikcja historyczna" , 25));
         ksiazkaRepository.save(new Ksiazka("Balladyna", "Juliusz S�owacki", "dramat" , 30));
         ksiazkaRepository.save(new Ksiazka("Ostatnie �yczenie", "Andrzej Sapkowski", "Powie��, Literatura fantasy", 28));
+
         
-      //  pracownikRepository.save(new Pracownik("Jakub", "Baran", 123456789, "administrator", a1 ));
+        pracownikRepository.save(new Pracownik("Jakub", "Baran", 123456789, "administrator", a1 ));
        // pracownikRepository.save(new Pracownik("Emilian", "Karnasiewicz", 345345345, "moderator" ));
       //  pracownikRepository.save(new Pracownik("Wojciech", "Gwizdak", 444444444, "logistyk" ));
       //  pracownikRepository.save(new Pracownik("Maciej", "Penar", 656565656, "moderator" ));
